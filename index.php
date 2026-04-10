fee371d4c5a54b0a8d6dbb1d4b0ff60f639eb9d6
Robert Vieira <vs.robert4@gmail.com>
2026-04-09 23:56:51 -0300

    Index correto

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
-    require_once './assets/config.php';
+    $titulo = "Titulo";
+    $descricao = "Descricao";
+    $nome = "Nome empresa";
+    $telefones = [
+        [
+            "nome" => "Nome",
+            "numero" => "(11) 99999-9999"
+        ],
+        [
+            "nome" => "Nome",
+            "numero" => "(11) 99999-9999"
+        ]
+    ];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <title><?php echo $titulo ?></title>
+    <meta name="description" content="<?php echo $descricao; ?>">
+    <title><?php echo $titulo; ?></title>
 </head>
 <body>
-    <h1>Telefones</h1>
-
-    <?php
-        foreach($telefones as $item){
-            echo "<p>" . $item["nome"] . "</p>";
-            echo "<p>" . $item["numero"] . "</p>";
-        };
-    ?>
+    
 </body>
 </html>
